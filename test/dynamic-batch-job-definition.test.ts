689855e497f7cdaf02921478f0afc6cd24a7d66f
Al Johri <aljohri@amazon.com>
2023-01-14 00:23:04 -0500

    Initial commit

---
+++ test/dynamic-batch-job-definition.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DynamicBatchJobDefinition from '../lib/dynamic-batch-job-definition-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamic-batch-job-definition-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamicBatchJobDefinition.DynamicBatchJobDefinitionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
